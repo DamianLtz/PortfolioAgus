5a443ab638368319f6eef22ef29c8f3b77a5cbfc
DamianLtz <77624662+DamianLtz@users.noreply.github.com>
2021-11-03 14:33:44 -0300

    v0.93

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,12 +1,8 @@
 import React from "react";
 
-const Button = ({ className, content, id, Flecha, onMouseOver }) => {
+const Button = ({ className, content, id, Flecha }) => {
   return (
-    <button
-      className={`button d-flex align-items-center ${className}`}
-      id={id}
-      onMouseOver={onMouseOver}
-    >
+    <button className={`button d-flex align-items-center ${className}`} id={id}>
       {content}
       <img src={Flecha} alt="" className="ms-2 flechita" />
     </button>
